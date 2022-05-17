9b5bcbd6b8ae4a5864718d5041d9686d978e1525
bhavesh-chabda-sams <bhavesh.chavda@solytics-partners.com>
2022-05-17 08:06:39 +0000

    File cleaner updated

--- file-cln.py
+++ file-cln.py
@@ -3,35 +3,48 @@ from os.path import exists
 from datetime import date,datetime,timedelta
 
 today_date  = date.today()
+
+
+#NOTE: CONCEPT
 yesterday = today_date - timedelta(days = 30)
 old_date = f'{yesterday.day}-{yesterday.month}-{yesterday.year}'
 old_month = f'{yesterday.month}-{yesterday.year}'
-# old_date
+
+del_dates1 = []
+del_dates2 = []
+for k in range(10,150):
+    ys = today_date - timedelta(days=k)
+    del_dates2.append(ys)
+    del_dates1.append(f'{ys.day}-{ys.month}-{ys.year}')
+
 
 base_dir = "/home/ubuntu/sanctions-scripts"
-len(os.listdir(base_dir))
-# del_dl1 = ["outputfiles","inputfiles","removedfiles","diffrancefiles"]
-# del_dl2 = ["OutputFiles","InputFiles","RemovedFiles","DifferenceFiles"]
-del_d1 = "diffrancefiles"
-del_d2 = "DifferenceFiles"
+del_fl1 = ["outputfiles","inputfiles","removedfiles","diffrancefiles"]
+del_fl2 = ["OutputFiles","InputFiles","RemovedFiles","DifferenceFiles"]
 c = 0
 for i in os.listdir(base_dir):
     try:
         if os.path.isdir(f"{base_dir}/{i}"):
             print(f"---\n-->{i}")
-            if del_d1 in os.listdir(f"{base_dir}/{i}"):
-                    for j in os.listdir(f"{base_dir}/{i}/{del_d1}"):
-                        if old_month in j:
-                            c+=1
-                            print(j)
-                            os.remove(f"{base_dir}/{i}/{del_d1}/{j}")
-                    # print("-----")1
-            elif del_d2 in os.listdir(f"{base_dir}/{i}"):
-                for j in os.listdir(f"{base_dir}/{i}/{del_d2}"):
-                    if old_month in j:
-                        c+=1
-                        print(j)
-                        os.remove(f"{base_dir}/{i}/{del_d2}/{j}")
+            # c+=1
+            for k in del_fl1:
+                if k in os.listdir(f"{base_dir}/{i}"):
+                    for del_d in del_dates1:
+                        for f in os.listdir(f"{base_dir}/{i}/{k}"):
+                            if f"{del_d}" in f:
+                                os.remove(f"{base_dir}/{i}/{k}/{f}")
+                                # print(f"{base_dir}/{i}/{k}/{f}")
+                                c+=1
+                                # print(f) 
+            for k in del_fl2:
+                if k in os.listdir(f"{base_dir}/{i}"):
+                    for del_d in del_dates2:
+                        for f in os.listdir(f"{base_dir}/{i}/{k}"):
+                            if f"{del_d}" in f:
+                                os.remove(f"{base_dir}/{i}/{k}/{f}")
+                                # print(f"{base_dir}/{i}/{k}/{f}")
+                                c+=1
+                                # print(f) 
                 # print("-----")
     except Exception as e:
         print(e)
